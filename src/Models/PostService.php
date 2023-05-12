04fe6cb011ef4abd4a7e9655aff95051761d8c47
poznerrr <pozner@gmail.com>
2023-05-12 23:24:09 +0300

    feat: add block for similar news

--- src/Models/PostService.php
+++ src/Models/PostService.php
@@ -150,4 +150,30 @@ class PostService
         return $posts;
     }
 
+    public function getTopSimilarPosts(int $currentId, string $search): array
+    {
+        $posts = [];
+        $query = "SELECT posts.post_id, posts.title, posts.post_text, posts.post_date, 
+            categories.category_name, categories.category_id, users.user_name, users.user_id
+            FROM posts
+                JOIN categories ON posts.category_id = categories.category_id
+                JOIN users ON posts.user_id = users.user_id
+            WHERE MATCH (title, post_text)
+                AGAINST ('$search' IN NATURAL LANGUAGE MODE) AND posts.post_id != $currentId
+            LIMIT 5";
+        $result = $this->db->query($query);
+        while ($row = $result->fetch()) {
+            $post = new Post;
+            $post->setId($row['post_id']);
+            $post->setCategory(new Category ($row['category_id'], $row['category_name']));
+            $post->setTitle($row['title']);
+            $post->setText($row['post_text']);
+            $post->setAuthor(new User($row['user_id'], $row['user_name']));
+            $post->setDate(date('Y-m-d H:i:s', $row['post_date']));
+
+            $posts[] = $post;
+        }
+        return $posts;
+    }
+
 }
--- src/Views/NewsView.php
+++ src/Views/NewsView.php
@@ -4,11 +4,12 @@ declare(strict_types=1);
 
 namespace Source\Views;
 
+use Source\App\UriMaker;
 use Source\Models\Post;
 
 class NewsView extends View
 {
-    public function __construct(protected string $domain, protected Post $post)
+    public function __construct(protected string $domain, protected Post $post, protected  array $similarNews, protected UriMaker $uriMaker)
     {
         parent::__construct();
         $this->mainPath = dirname(__DIR__) . '/Layouts/news.phtml';
